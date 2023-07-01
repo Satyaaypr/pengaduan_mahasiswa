dda66d41afd18644679b3a1f9b9c8fa1d839520c
Satyaaypr <satyaaypr@gmail.com>
2023-07-01 17:06:29 +0700

    Perubahan pertama

---
+++ login.php
@@ -0,0 +1,61 @@
+<div class="card" style="padding: 50px; width: 60%; margin: 0 auto; margin-top: 10%;">
+<h4 style="text-align: center;" class="orange-text">Sistem Pengaduan Mahasiswa</h4><br>
+
+	<form method="POST">
+		<div class="input_field">
+			<label for="username">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  					<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+				</svg></i> Username </label>
+			<input id="username" type="text" name="username" required>
+		</div>
+		<div class="input_field">
+			<label for="password">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+  					<path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+				</svg> Password</label>
+			<input id="password" type="password" name="password" required>
+		</div>
+		<input type="submit" name="login" value="Login" class="btn orange" style="width: 100%;">
+	</form>
+</div>
+<?php 
+	if(isset($_POST['login'])){
+		$username = mysqli_real_escape_string($koneksi,$_POST['username']);
+		$password = mysqli_real_escape_string($koneksi,md5($_POST['password']));
+	
+		$sql = mysqli_query($koneksi,"SELECT * FROM mahasiswa WHERE username='$username' AND password='$password' ");
+		$cek = mysqli_num_rows($sql);
+		$data = mysqli_fetch_assoc($sql);
+	
+		$sql2 = mysqli_query($koneksi,"SELECT * FROM petugas WHERE username='$username' AND password='$password' ");
+		$cek2 = mysqli_num_rows($sql2);
+		$data2 = mysqli_fetch_assoc($sql2);
+
+		if($cek>0){
+			session_start();
+			$_SESSION['username']=$username;
+			$_SESSION['data']=$data;
+			$_SESSION['level']='mahasiswa';
+			header('location:mahasiswa/');
+		}
+		elseif($cek2>0){
+			if($data2['level']=="admin"){
+				session_start();
+				$_SESSION['username']=$username;
+				$_SESSION['data']=$data2;
+				header('location:admin/');
+			}
+			elseif($data2['level']=="petugas"){
+				session_start();
+				$_SESSION['username']=$username;
+				$_SESSION['data']=$data2;
+				header('location:petugas/');
+			}
+		}
+		else{
+			echo "<script>alert('Login gagal. Username atau Password anda salah.')</script>";
+		}
+
+	}
+ ?>
